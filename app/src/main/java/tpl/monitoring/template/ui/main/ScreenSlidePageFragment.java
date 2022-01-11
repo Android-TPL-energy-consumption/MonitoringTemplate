d6ee10292d2e3cf0ea33a79fbefb374ec539b487
Remy Raes <raes.remy@gmail.com>
2022-01-11 10:16:17 +0100

    [feat] each fragment has its own icon

--- app/src/main/java/tpl/monitoring/template/ui/main/ScreenSlidePageFragment.java
+++ app/src/main/java/tpl/monitoring/template/ui/main/ScreenSlidePageFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import tpl.monitoring.template.R;
 import tpl.monitoring.template.databinding.FragmentScreenSlidePageBinding;
 
 public class ScreenSlidePageFragment extends Fragment {
@@ -17,11 +18,29 @@ public class ScreenSlidePageFragment extends Fragment {
         this._position = position;
     }
 
+    public int _getFragmentIcon() {
+        switch (this._position) {
+            case 0:
+                return R.drawable.ic_baseline_home_24;
+            case 1:
+                return R.drawable.ic_baseline_star_24;
+            case 2:
+                return R.drawable.ic_baseline_airplanemode_active_24;
+            case 3:
+                return R.drawable.ic_baseline_favorite_24;
+            case 4:
+                return R.drawable.ic_baseline_battery_charging_full_24;
+            default:
+                return R.drawable.ic_launcher_background;
+        }
+    }
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         FragmentScreenSlidePageBinding binding = FragmentScreenSlidePageBinding.inflate(inflater);
         binding.slidePosition.setText(String.valueOf(_position));
+        binding.slideLogo.setImageResource(_getFragmentIcon());
         return binding.getRoot();
     }
 }
